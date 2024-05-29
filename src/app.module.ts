385e5ea458a8c64406ed922bb1a9a7c4e0f970a8
jorgehernandez16 <jhernandezmorelo16@correo.unicorodba.edu.co>
2024-05-29 13:20:54 -0500

    cambio de database

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { LanguagesModule } from './languages/languages.module';
 import { MongooseModule } from '@nestjs/mongoose'; 
 
 @Module({
-  imports: [CommandsModule, ElementsModule, LanguagesModule, MongooseModule.forRoot('mongodb://jorged:1003345316@ac-sjxqnea-shard-00-00.f67wnfv.mongodb.net:27017,ac-sjxqnea-shard-00-01.f67wnfv.mongodb.net:27017,ac-sjxqnea-shard-00-02.f67wnfv.mongodb.net:27017/commands?replicaSet=atlas-afw8ku-shard-0&ssl=true&authSource=admin')],
+  imports: [CommandsModule, ElementsModule, LanguagesModule, MongooseModule.forRoot('mongodb://jorged:1003345316@ac-sjxqnea-shard-00-00.f67wnfv.mongodb.net:27017,ac-sjxqnea-shard-00-01.f67wnfv.mongodb.net:27017,ac-sjxqnea-shard-00-02.f67wnfv.mongodb.net:27017/julia?replicaSet=atlas-afw8ku-shard-0&ssl=true&authSource=admin')],
   controllers: [AppController],
   providers: [AppService],
 })
