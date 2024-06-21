385ec731e776d13b77f652947f85d2de34e335f5
jorgehernandez16 <jhernandezmorelo16@correo.unicorodba.edu.co>
2024-06-21 13:11:18 -0500

    Documentacion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as path from 'path'
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
+  
 }
 bootstrap();
